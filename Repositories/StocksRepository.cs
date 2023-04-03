d0dce1897aa80c626dac4072de44cbb619275840
Rogeio Garcia <rogeliogarciag@gmail.com>
2023-04-02 20:13:47 -0600

    Se agregan las soluciones Repositories y RepositoryContracts

---
+++ Repositories/StocksRepository.cs
@@ -0,0 +1,29 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace Repositories
+{
+    public class StocksRepository : IStocksRepository
+    {
+        private readonly ApplicationDBContext _db;
+        public async Task<BuyOrder> CreateBuyOrder(BuyOrder buyOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<SellOrder> CreateSellOrder(SellOrder sellOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<BuyOrder>> GetBuyOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<SellOrder>> GetSellOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
